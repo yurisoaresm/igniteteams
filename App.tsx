20bbcecac06298c18a0a81273e8ff873bf413d98
Yuri Soares <yurisoaresm@outlook.com>
2023-03-03 17:25:37 -0300

    build/feat: add expo-fonts-google dependence and config custom fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
 import Groups from '@screens/Group';
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <>
-      <Groups />
-    </>
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups /> : <Loading /> }
+    </ThemeProvider>
   );
 }
