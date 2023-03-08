64e512615b0c66c3b594ecea6e7f3871527cc044
Yuri Soares <yurisoaresm@outlook.com>
2023-03-08 16:23:48 -0300

    feat: create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,21 @@
+import { TouchableOpacityProps } from 'react-native';
+
+import { Container, Title, ButtonTypeStyleProps } from './styles';
+
+type Props = {
+  title: string;
+  type?: ButtonTypeStyleProps
+} & TouchableOpacityProps;
+
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
+  return (
+    <Container
+      type={type}
+      {...rest}
+    >
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  );
+}
--- src/screens/Group/index.tsx
+++ src/screens/Group/index.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
@@ -13,7 +20,25 @@ export default function Groups() {
         subtitle="Jogue com a sua turma"
       />
 
-      <GroupCard title='Galera do adoradores'/>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Tente cadastrar uma nova turma" 
+          />
+        )}
+      />
+
+      <Button 
+        title="Criar nova turma"
+      />
     </Container>
   );
 }
