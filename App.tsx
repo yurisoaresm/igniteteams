0c8d57defb7eddfdb1d9c9662b3ef3873df4c530
Yuri Soares <yurisoaresm@outlook.com>
2023-03-09 16:46:34 -0300

    feat: create Players page with input

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { Loading } from '@components/Loading';
 
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 import theme from './src/theme';
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <NewGroup /> : <Loading /> }
+      { fontsLoaded ? <Players /> : <Loading /> }
     </ThemeProvider>
   );
 }
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,31 @@
+import { Input } from '@components/Input';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+
+import { Container, Form } from './styles';
+
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight 
+        title="Nome da turma"
+        subtitle="Adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon
+          icon="add"
+        />
+      </Form>
+
+    </Container>
+  );
+}
